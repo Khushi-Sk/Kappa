dc15ff0c1bfeeb3b8a23bcbc0172a48e0b7a48a1
Khushi-Sk <khushisukar234@gmail.com>
2025-02-19 15:27:50 +0000

    responsive form

--- kappa/src/app/contact/page.js
+++ kappa/src/app/contact/page.js
@@ -19,23 +19,23 @@ export default async function Contact() {
     return (<>
     
     <div className="lg:flex lg:justify-center lg:py-5">
-        <form action={handleSubmit} className="p-5 m-5 flex flex-col justify-center gap-5 bg-purple-300 text-black w-sm md:w-[800px] rounded-xl">
-            <div className=" flex flex-row gap-10 ">
+        <form action={handleSubmit} className="p-5 m-5 flex-col flex sm:flex-col gap-5 bg-purple-300 text-black w-sm md:w-[800px] rounded-xl">
+            <div className=" flex flex-col sm:flex-row sm:gap-10 ">
                 <label className="p-1">Name</label>
                 <input name="name" className="outline-none p-1 rounded-md" type="text"  placeholder="Name" required/>
             </div>
 
-            <div className="flex flex-row gap-11">
+            <div className="flex flex-col sm:flex-row sm:gap-11">
                 <label className="p-1">Email</label>
                 <input name="email" placeholder="Email" type="email" className="outline-none p-1 rounded-md" required/>
             </div>
             
-            <div className=" flex flex-row gap-2">
+            <div className=" flex flex-col sm:flex-row sm:gap-2">
                 <label className="p-1">Mobile No.</label>
-                <input name="mobile" placeholder="Mobile No." type="tel" className="outline-none p-1 rounded-md" required/>
+                <input name="mobile" placeholder="Mobile No." type="tel" className="outline-none p-1 rounded-md " required/>
             </div>
             
-            <div className=" flex flex-row gap-5">
+            <div className=" flex flex-col sm:flex-row sm:gap-5">
                 <label className="p-1">Message</label>
                 <textarea name="message" placeholder="Message" type='text' cols={40} rows={5} className="outline-none p-1 rounded-md" required></textarea>
             </div>
